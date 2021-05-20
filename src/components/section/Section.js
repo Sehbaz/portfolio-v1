a42e104eddebc5eb3e285ff47571bf489d1e57e0
Sehbaz Rafik <sehbazm@gmail.com>
2021-05-20 23:57:27 +0100

    home section updated

--- src/components/section/Section.js
+++ src/components/section/Section.js
@@ -4,11 +4,14 @@ function Section() {
   return (
     <div className="section-container">
       {" "}
-      <div id="about" style={{ height: 500 }}>
-        <h1>This is About section</h1>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Id,
-        repellendus. Totam nihil similique a repellat minus dolor amet quasi.
-        Corporis nulla quaerat iste, sed quasi ab dolorem maxime minima animi.
+      <div id="about" className="about-section">
+        <p className="green-text">Hi I am,</p>
+        <p className="heading-1">Sehbaz Rafik</p>
+        <p className="heading-2">I build things for the web.</p>
+        <p className="heading-3">
+          I aim to create more free of ads, more sustainable, more scalable and
+          profitable apps.
+        </p>
       </div>
       <div id="work" style={{ height: 500 }}>
         <h1>This is Service section</h1>
