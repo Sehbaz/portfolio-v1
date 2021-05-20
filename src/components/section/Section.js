c4dc7325ba27bb8477266274dbcc9198a635b15a
Sehbaz Rafik <sehbazm@gmail.com>
2021-05-21 00:24:52 +0100

    about section updated

--- src/components/section/Section.js
+++ src/components/section/Section.js
@@ -5,7 +5,7 @@ function Section() {
     <div className="section-container">
       {" "}
       <div id="about" className="about-section">
-        <p className="green-text">Hi I am,</p>
+        <p className="green-text">Hi, I am</p>
         <p className="heading-1">Sehbaz Rafik</p>
         <p className="heading-2">I build things for the web.</p>
         <p className="heading-3">
@@ -19,11 +19,34 @@ function Section() {
         repellendus. Totam nihil similique a repellat minus dolor amet quasi.
         Corporis nulla quaerat iste, sed quasi ab dolorem maxime minima animi.
       </div>
-      <div id="contact" style={{ height: 500 }}>
-        <h1>This is Contact section</h1>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Id,
-        repellendus. Totam nihil similique a repellat minus dolor amet quasi.
-        Corporis nulla quaerat iste, sed quasi ab dolorem maxime minima animi.
+      <div id="contact" className="contact-section">
+        <p className="green-text">03. What’s Next?</p>
+        <span className="heading-1">Around the web</span>
+        <span className="contact-text">
+          My inbox is always open. Whether you have a question or just want to
+          say hi, I'll try my best to get back to you! Say Hello
+        </span>
+        <br />
+        <div>
+          <span className="heading-3">
+            Email :
+            <a href="mailto:sehbazm@gmail.com" className="link">
+              <span className="green-text"> sehbazm@gmail.com </span>
+            </a>
+          </span>
+
+          <br />
+          <span className="heading-3">
+            GitHub :
+            <a
+              href="https://github.com/Sehbaz"
+              target="_blanc"
+              className="link"
+            >
+              <span className="green-text"> Sehbaz </span>
+            </a>
+          </span>
+        </div>
       </div>
     </div>
   );
